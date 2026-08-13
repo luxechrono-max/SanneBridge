10a9c5e7416dbb5dac57c5c40119474df86f3402
luxechrono-max <luxechronowatches@gmail.com>
2026-08-13 10:40:54 +0200

    Update settings.tsx

--- src/settings.tsx
+++ src/settings.tsx
@@ -11,19 +11,39 @@ export default () => {
                 label="SanneBridge"
                 leading={
                     <FormIcon
-                        source={getAssetIDByName("voice_bar_mute_off")}
+                        source={getAssetIDByName(
+                            "voice_bar_mute_off"
+                        )}
                     />
                 }
             />
 
             <FormDivider />
 
-            <FormRow
-                label="TEST SANNE BUTTON"
-                onPress={() => {
-                    console.log("[SanneBridge] button pressed");
+            <ReactNative.TouchableOpacity
+                onPress={() =>
+                    console.log(
+                        "[SanneBridge] BUTTON PRESSED"
+                    )
+                }
+                style={{
+                    padding: 16,
+                    marginHorizontal: 12,
+                    marginVertical: 8,
+                    borderRadius: 8,
+                    backgroundColor: "#5865f2",
                 }}
-            />
+            >
+                <ReactNative.Text
+                    style={{
+                        color: "white",
+                        textAlign: "center",
+                        fontWeight: "700",
+                    }}
+                >
+                    TEST SANNE BUTTON
+                </ReactNative.Text>
+            </ReactNative.TouchableOpacity>
         </ReactNative.ScrollView>
     );
 };
