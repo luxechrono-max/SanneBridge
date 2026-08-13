05ffaec517096281141c8f2007baa4023cf52d33
luxechrono-max <luxechronowatches@gmail.com>
2026-08-13 14:48:28 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,37 @@
-import { startChannelMenu } from "./channelMenu";
-
-let stop: (() => void) | null = null;
+import { findAllExports } from "@metro/finders";
+import { createSimpleFilter } from "@metro/factories";
+import { ReactNative } from "@metro/common";
 
 export const onLoad = () => {
-    stop = startChannelMenu();
+    const found = findAllExports(
+        createSimpleFilter(
+            (m: any) => {
+                if (!m || typeof m !== "object") return false;
+
+                return Object.keys(m).some((key) =>
+                    /attachment|upload|file|composer|messageInput/i.test(key)
+                );
+            },
+            "sanne-runtime-upload-search"
+        )
+    );
+
+    const results = found
+        .map((m: any) =>
+            Object.keys(m).filter((key) =>
+                /attachment|upload|file|composer|messageInput/i.test(key)
+            )
+        )
+        .flat();
+
+    ReactNative.Alert.alert(
+        "SanneBridge",
+        results.length
+            ? [...new Set(results)].slice(0, 80).join("\n")
+            : "NO MATCHING MODULE PROPERTIES"
+    );
 };
 
-export const onUnload = () => {
-    stop?.();
-    stop = null;
-};
+export const onUnload = () => {};
 
 export { default as settings } from "./settings";
