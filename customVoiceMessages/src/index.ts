9cc6c539780bd8b21b431eacea12a8e477bc1d72
luxechrono-max <luxechronowatches@gmail.com>
2026-08-14 12:15:29 +0200

    Update index.ts

--- customVoiceMessages/src/index.ts
+++ customVoiceMessages/src/index.ts
@@ -15,7 +15,10 @@ export const onLoad = () => {
     try {
         patches.push(voiceMessages());
     } catch (e) {
-        console.log("[CustomVoiceMessages+] voiceMessages failed:", e);
+        console.log(
+            "[CustomVoiceMessages+] voiceMessages failed:",
+            e
+        );
     }
 
     try {
@@ -23,7 +26,10 @@ export const onLoad = () => {
         patches.push(msgSuccess());
         patches.push(msgUpdate());
     } catch (e) {
-        console.log("[CustomVoiceMessages+] messagePatches failed:", e);
+        console.log(
+            "[CustomVoiceMessages+] messagePatches failed:",
+            e
+        );
     }
 };
 
